a028d9e033e824cb0e9c54bbe2c87d0c0badff7c
dihbgs <dihborgesdr@gmail.com>
2025-03-16 17:03:03 -0300

    update: complete exercise 0.3

--- chapter-00/Walker.js
+++ chapter-00/Walker.js
@@ -1,8 +1,9 @@
 class Walker {
-  constructor(speed) {
+  constructor() {
+    this.speed = 8;
     this.x = width / 2;
     this.y = height / 2;
-    this.speed = speed;
+    this.mouseProbability = 0;
     this.color = [random(360), 120 + random(60), 100 + random(80)];
   }
 
@@ -12,10 +13,20 @@ class Walker {
   }
 
   step() {
-    let xstep = random(-1, 1);
-    let ystep = random(-1, 1);
+    const xstep = random(-this.speed, this.speed);
+    const ystep = random(-this.speed, this.speed);
+    const moveToMouse = random() < this.mouseProbability / 100;
+    const mouseDirection = [mouseX - this.x, mouseY - this.y];
+    const magnitude = Math.sqrt(
+      Math.pow(mouseDirection[0], 2) + Math.pow(mouseDirection[1], 2)
+    );
 
-    this.x += xstep * this.speed;
-    this.y += ystep * this.speed;
+    if (moveToMouse) {
+      this.x += (mouseDirection[0] / magnitude) * this.speed;
+      this.y += (mouseDirection[1] / magnitude) * this.speed;
+    } else {
+      this.x += xstep;
+      this.y += ystep;
+    }
   }
 }
--- chapter-00/walker-tester.js
+++ chapter-00/walker-tester.js
@@ -4,19 +4,34 @@ function setup() {
   createCanvas(640, 512);
 
   for (let i = 0; i < 64; i++) {
-    walkers[i] = new Walker(random(8));
+    walkers[i] = new Walker();
   }
 
   colorMode("HSB");
 
-  frameRate(30);
+  frameRate(60);
   strokeWeight(16);
-  background(33);
+  background(33, 33, 33, 255);
 }
 
 function draw() {
+  background(33, 33, 33, 255 / 16);
   for (let i = 0; i < walkers.length; i++) {
     walkers[i].step();
     walkers[i].show();
+  }
+}
+
+function mouseDragged() {
+  for (let i = 0; i < walkers.length; i++) {
+    walkers[i].mouseProbability += 0.5;
+    walkers[i].speed += 0.1;
+  }
+}
+
+function mouseReleased() {
+  for (let i = 0; i < walkers.length; i++) {
+    walkers[i].mouseProbability = 0;
+    walkers[i].speed = 4;
   }
 }
